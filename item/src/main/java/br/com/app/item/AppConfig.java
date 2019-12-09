2f731feee9620aeacf92b2c960df32f7d9e18dbd
Leandro Silva <lsilva.info@gmail.com>
2019-12-09 14:23:57 -0300

    Configuração Eureka Server

--- item/src/main/java/br/com/app/item/AppConfig.java
+++ item/src/main/java/br/com/app/item/AppConfig.java
@@ -1,13 +1,11 @@
 package br.com.app.item;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@RibbonClient(name = "produto")
+
 @Configuration
 public class AppConfig {
 
